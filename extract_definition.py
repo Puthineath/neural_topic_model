6437fd16794b84a3d63c7ceb79644b560ae60c39
Puthineath_Lay <puthineath.lay@tracksens.com>
2021-06-26 09:53:40 +0200

    preprocessing

--- extract_definition.py
+++ extract_definition.py
@@ -1,6 +1,7 @@
 import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 import spacy
+from pprint import  pprint
 from string import punctuation
 
 nlp = spacy.load('en_core_web_sm')
@@ -11,54 +12,64 @@ query = """
             PREFIX euvoc: <http://publications.europa.eu/ontology/euvoc#>
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-            SELECT ?Definition
+            SELECT ?definition ?value ?concept
             FROM <http://www.data.com/3>
             WHERE {
-                ?c euvoc:xlDefinition ?a .
-                ?a rdf:value ?Definition.
-                ?c skos:prefLabel "EU financing"@en .
+                ?concept euvoc:xlDefinition ?definition .
+                ?definition rdf:value ?value.
+                ?concept skos:prefLabel "EU financing"@en .
             }
             LIMIT 25
         """
-
+# query1 = """
+#     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+#     PREFIX euvoc: <http://publications.europa.eu/ontology/euvoc#>
+#     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#
+#     SELECT ?value ?definition ?label
+#     FROM <http://www.data.com/3>
+#     WHERE {
+#
+#       <http://eurovoc.europa.eu/1005> euvoc:xlDefinition ?definition .
+#       ?definition rdf:value ?value.
+#       <http://eurovoc.europa.eu/1005> skos:prefLabel ?label .
+#       filter(lang(?definition) = "en")
+#     }
+#     LIMIT 25
+# """
 def get_results(endpoint_url, query):
     user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
     # TODO adjust user agent; see https://w.wiki/CX6
     sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
     sparql.setQuery(query)
+    # sparql.setQuery(query % {'concept': str(concept)})
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
-# def get_hotwords(text):
-# #     result = []
-# #     pos_tag = ['PROPN', 'ADJ', 'NOUN']  # 1
-# #     doc = nlp(text.lower())  # 2
-# #     for token in doc:
-# #         # remove stop word and punctuation
-# #         if (token.text in nlp.Defaults.stop_words or token.text in punctuation):
-# #             continue
-# #         # keep only pronounce, adjective, and noun
-# #         if (token.pos_ in pos_tag):
-# #             result.append(token.text)
-# #     return result
-def get_hotwords(texts):
-    doc = nlp(texts)
+def preprocessing(texts):
+    # remove stop words
+    sw_spacy = nlp.Defaults.stop_words
+    words = [word for word in texts.split() if word.lower() not in sw_spacy]
+    new_text = " ".join(words)
+    # get keywords
+    doc = nlp(new_text)
+    # need to convert to string, not list
     a = [chunk.text for chunk in doc.noun_chunks]
     return a
 
 def main():
+
     list_of_definition = []
-    list_of_lang = []
     results = get_results(endpoint_url, query)
+    # get keywords from each dictionary
     for result in results["results"]["bindings"]:
-        # get the definition for each language
-        list_of_definition.append(result['Definition']['value'])
-        #
-        list_of_lang.append(result['Definition']['xml:lang'])
-    dic_definiton = dict(zip(list_of_lang, list_of_definition))
-    for key, value in dic_definiton.items():
-        print("{}:{}".format(key, get_hotwords(value)))
+        result['value']['value'] = preprocessing(result['value']['value'])
+        # print(result)
+    pprint(results)
+    # convert_definition(list)
+
 
 if __name__ == "__main__":
+    # print(main())
     main()
 
