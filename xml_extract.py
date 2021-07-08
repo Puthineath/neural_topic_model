5e533bfbfb44c39eee5877589bfddbd2d5465e36
Puthineath_Lay <puthineath.lay@tracksens.com>
2021-07-08 16:48:29 +0200

    merge xml file and save to csv

--- xml_extract.py
+++ xml_extract.py
@@ -1,3 +1,7 @@
+ # extract data from XML files
+import csv
+import glob
+import os
 from bs4 import BeautifulSoup as bs
 import spacy
 from pprint import pprint
@@ -5,15 +9,10 @@ from pprint import pprint
 nlp = spacy.load('en_core_web_sm')
 content = []
 result_list = []
-num_datas = ["CELEX - 32006D0199","CELEX - 32017R1004","CELEX - 32019R0833","CELEX - 32013R1303","CELEX - 32019R0473"]
+folders = ["CELEX - 32017R1004","CELEX - 32019R0833","CELEX - 32013R1303","CELEX - 32019R0473"]
 tag_names = ["ti.art","title","sti.art"]
 
-# need to extract the label
-
-def add_path(data):
-    # ***** need to rename the doc file in the folder by removing (1),(2),...
-    PATH = "data/" + data + "/DOC_2.xml"
-    return PATH
+# ***** need to extract the labels from MongoDB
 
 def pre_processing(texts):
     # remove stop words
@@ -23,17 +22,23 @@ def pre_processing(texts):
     # get keywords
     doc = nlp(new_text)
     a = [chunk.text for chunk in doc.noun_chunks]
-
     # need to remove the word "article" + number and remove word annex
     # need to remove punctutation
-
     return " ".join(a)
+
+def merge_files(folder):
+    # get all xml files in each folder
+    files = glob.glob('data/'+folder+'/*.xml')
+    # merge
+    with open("data/"+folder+"/concat.xml", "wb") as outfile:
+        for f in files:
+            with open(f, "rb") as infile:
+                outfile.write(infile.read())
+    return
 
 def get_data(file):
     data = {}
-
     with open(file, "r", encoding="utf-8") as file:
-
         # Read each line in the file, readlines() returns a list of lines
         content = file.readlines()
 
@@ -45,9 +50,12 @@ def get_data(file):
         data['title'] = bs_content.find("title").get_text()
 
         #  remove unimportant tag(s) to get the important info in the article
-        for tag_name in tag_names:
-            for tag in bs_content.find("enacting.terms")(tag_name):
-                tag.decompose()
+        try:
+            for tag_name in tag_names:
+                for tag in bs_content.find("enacting.terms")(tag_name):
+                    tag.decompose()
+        except:
+            pass
 
         # get the article
         data['article'] = bs_content.find("enacting.terms").get_text()
@@ -58,10 +66,20 @@ def get_data(file):
         return data
 
 def main():
-    for num_data in num_datas:
-        result_list.append(get_data(add_path(num_data)))
-    print(result_list)
+    for folder in folders:
+        # merge all files in each folder
+        merge_files(folder)
+        # put the all documents to the list
+        result_list.append(get_data("data/"+folder+"/concat.xml"))
 
+    # save to csv file
+    a_file = open("data/data.csv", "w")
+    for result in result_list:
+        writer = csv.writer(a_file)
+        for key, value in result.items():
+            writer.writerow([key, value])
+
+    a_file.close()
 
 if __name__ == '__main__':
     main()
