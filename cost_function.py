9dbc0f472cdfcdcc4a9bad9de644e74a380ceb34
tracksens <salbouze@tracksens.com>
2021-08-08 17:07:16 +0200

    intermediate state

---
+++ cost_function.py
@@ -0,0 +1,33 @@
+
+import numpy
+
+
+# Initialisation of the W2 Matrix - first with 0
+
+w2 = ""
+le = ""
+
+K = 5
+
+
+def w2_initialisation():
+    global w2
+    line = []
+    for i in range(0, K-1):
+        line.append("0")
+    w2 = numpy.array
+    return
+
+
+def le_data_loading():
+    return
+
+
+def cost_function():
+    le_data_loading()
+    w2_initialisation()
+    return
+
+
+if __name__ == '__main__':
+    cost_function()
